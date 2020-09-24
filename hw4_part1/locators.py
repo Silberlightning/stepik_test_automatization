a5fc34926382cc7548a21f5d49436e665ea677a0
OlMe <spogulit@gmail.com>
2020-09-24 20:26:01 +0200

    Homework Week4 Part1

---
+++ hw4_part1/locators.py
@@ -0,0 +1,5 @@
+link = "http://selenium1py.pythonanywhere.com"
+search_input = "//input[@type='search']"
+search_text ="tattoo"
+button_search = "input.btn.btn-default"
+button = "//button[@type='submit']"
---
+++ hw4_part1/test_items.py
@@ -0,0 +1,18 @@
+import locators as locators
+from selenium import webdriver
+
+def test_button_add_to_cart(browser):
+
+    #Arrange
+    browser.get(locators.link)
+    search_input = browser.find_element_by_xpath(locators.search_input)
+
+    #Act
+    search_input.send_keys(locators.search_text)
+    browser.find_element_by_css_selector(locators.button_search).click()
+
+    #Assert
+    button_to_cart = browser.find_element_by_xpath(locators.button).text
+    assert len(button_to_cart) != 0, "Button text should not be null"
+
+    browser.quit()
